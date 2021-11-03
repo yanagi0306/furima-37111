589d2b017ea9e46a846c2d6ddeedea1e3b278b48
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-11-03 17:17:28 +0900

    model validateActiveHash::Baseクラス作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to: :prefecture
+  belongs_to: :status
+  belongs_to: :shipping_bearer
+  belongs_to: :shipping_day
+  belongs_to: :category_name
+
+
+  validates :name,:description,presence: true
+
+  validates :status_id,numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  validates :shipping_bearer_id,numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  validates :shipping_day_id,numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  validates :prefecture_id, numericality: { only_integer: true,greater_than_or_equal_to: 2, less_than_or_equal_to: 48 }
+
+  validates :category_name_id, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+
+  validates :price, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
 end
