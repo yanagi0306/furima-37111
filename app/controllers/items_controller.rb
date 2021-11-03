e06876592b111c75bb7d6d59187d8adb7bcfc60f
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-11-03 17:40:00 +0900

    item#new作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,11 @@ class ItemsController < ApplicationController
   def index
   end
   def new
+    @item = Item.new
 
+  end
+  private
+  def item_params
+    params.require(:item).parameter(:name,:description,:status_id,:shipping_bearer_id,:shipping_day_id,:price,:prefecture_id,:category_name_id)
   end
 end
