08b4b0a69088fc09b19d3a9247c41f0ec3cf36bf
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-11-04 13:12:18 +0900

    変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    @items = Item.order('created_at DESC').limit(5)
+    @items = Item.order('created_at DESC')
   end
 
   def new
