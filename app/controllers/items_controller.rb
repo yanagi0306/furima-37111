37c6587d8ec8cccddf34026ace08cbd18a029028
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-11-04 17:20:17 +0900

    Revert "Merge pull request #17 from yanagi0306/商品削除機能"
    This reverts commit 82a6db7fdd8e97886f93d7ac9bba0c8853ebce1a, reversing
    changes made to 8e8d73a23fbfbcc8ee6c5dc451446afdd0fcd9e3.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :set_item, only: [:edit, :update,:show,:destroy]
-  before_action :check_user, only: [:edit,:update,:destroy]
+  before_action :set_item, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -21,7 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-
+    unless @item.user.id == current_user.id
+      redirect_to root_path
+    end
   end
 
   def update
@@ -31,20 +32,8 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-  def show
-
-  end
-  def destroy
-    @item.destroy
-    redirect_to root_path
-  end
 
   private
-  def check_user
-    unless @item.user.id == current_user.id
-      redirect_to root_path
-    end
-  end
 
   def set_item
     @item = Item.find(params[:id])
